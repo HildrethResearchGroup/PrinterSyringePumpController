648ec81f441301afd2ea0c4720a98fd6fa30e2c0
Alyssa Hanson <abhanson@mines.edu>
2024-06-03 12:10:18 -0600

    Added more functionality for operations

--- Sources/PrinterController/PrinterController/PrinterController+SyringePump.swift
+++ Sources/PrinterController/PrinterController/PrinterController+SyringePump.swift
@@ -9,65 +9,28 @@ import Foundation
 
 
 public extension PrinterController {
-  func setFlowRate1(to flowRate1: Double) async throws {
-    try await with(.syringePump) {
-      try await waveformController?.setRawVoltage(to: flowRate1)
-    }
-  }
+//  func setFlowRate1(to flowRate1: Double) async throws {
+//    try await with(.pump) {
+//      try await syringePumpController?.setRawVoltage(to: flowRate1)
+//    }
+//  }
+//  
+//    func setFlowRate2(to flowRate2: Double) async throws {
+//      try await with(.pump) {
+//        try await waveformController?.setRawVoltage(to: flowRate1)
+//      }
+//    }
   
-    func setFlowRate2(to flowRate2: Double) async throws {
-      try await with(.syringePump) {
-        try await waveformController?.setRawVoltage(to: flowRate1)
-      }
-    }
-    
-    func setAmplifiedVoltage(to voltage: Double) async throws {
-        try await with(.waveform) {
-            try await waveformController?.setAmplifiedVoltage(to: voltage)
-        }
-    }
-    
-    func setAmplifiedVoltageOffset(to offset: Double) async throws {
-        try await with(.waveform) {
-            try await waveformController?.setAmplifiedVoltageOffset(to: offset)
-        }
-    }
-  
-  func setFrequency(to frequency: Double) async throws {
-    try await with(.waveform) {
-      try await waveformController?.setFrequency(to: frequency)
-    }
-  }
-  
-  func setPhase(to phase: Double) async throws {
-    try await with(.waveform) {
-      try await waveformController?.setPhase(to: phase)
-    }
-  }
-  
-  func setWaveFunction(to waveFunction: WaveFunction) async throws {
-    try await with(.waveform) {
-      try await waveformController?.setWaveFunction(to: waveFunction)
-    }
-  }
-    
-    func setImpedance(to impedance: Double) async throws {
-        try await with(.waveform) {
-            try await waveformController?.setImpedance(to: impedance)
-        }
-    }
-  
-  func turnVoltageOn() async throws {
-    try await with(.waveform) {
-            await setWaveformState(\.voltageIsOn, to: true)
-      try await waveformController?.turnVoltageOn()
-    }
-  }
-  
-  func turnVoltageOff() async throws {
-    try await with(.waveform) {
-            await setWaveformState(\.voltageIsOn, to: false)
-      try await waveformController?.turnVoltageOff()
-    }
-  }
+//    func turnPumpOn(pump: String) async throws {
+//    try await with(.pump) {
+//      try await SyringePumpController?.startPumping()
+//    }
+//  }
+//  
+//  func turnPumpOff() async throws {
+//    try await with(.pump) {
+//            await setWaveformState(\.voltageIsOn, to: false)
+//      try await waveformController?.turnVoltageOff()
+//    }
+//  }
 }
