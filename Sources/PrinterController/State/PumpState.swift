a43d20750e69ea7641f5d9e21858641fdd3259f7
E-L-Cook <ecook@mines.edu>
2024-06-03 14:24:23 -0600

    copy pasted waveform state as basis for Pump state. Not finished

--- Sources/PrinterController/State/PumpState.swift
+++ Sources/PrinterController/State/PumpState.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+//TODO: change the copy pasted-code so that it actually works with the
+public struct PumpState {
+  public var updateInterval: TimeInterval? = 0.2
+  
+  public internal(set) var rawVoltage: Double?
+  public internal(set) var rawVoltageOffset: Double?
+  public internal(set) var frequency: Double?
+  public internal(set) var phase: Double?
+  public internal(set) var waveFunction: WaveFunction?
+    public internal(set) var impedance: Double?
+    public internal(set) var voltageIsOn: Bool?
+    
+    public var amplifiedVoltage: Double? {
+        rawVoltage.flatMap { $0 * 1000 }
+    }
+    
+    public var amplifiedVoltageOffset: Double? {
+        rawVoltageOffset.flatMap { $0 * 1000 }
+    }
+}
